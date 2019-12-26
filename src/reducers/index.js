17f9ad00ddeec536b040fe84d17d3679eb34128b
William Blacknall <williamblacknall@gmail.com>
2019-12-26 16:19:57 -0500

    Initial commit and layout for Redux added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,23 +1,23 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from "redux";
 
 const songsReducer = () => {
-    return [
-        {title: 'No Scrubs', duration: '4:05'},
-        {title: "Macarena", duration : "2:30"},
-        {title: "All Star", duration: "3:15"},
-        {title: "I Want it that Way", duration: "1:45"}
-    ]
-}
+	return [
+		{ title: "No Scrubs", duration: "4:05" },
+		{ title: "Macarena", duration: "2:30" },
+		{ title: "All Star", duration: "3:15" },
+		{ title: "I Want it that Way", duration: "1:45" }
+	];
+};
 
-const selectedSongReducer = (selectedSong=null, action) => {
-    if(action.type === "SONG_SELECTED") {
-        return action.payload;
-    }
+const selectedSongReducer = (selectedSong = null, action) => {
+	if (action.type === "SONG_SELECTED") {
+		return action.payload;
+	}
 
-    return selectedSong
-}
+	return selectedSong;
+};
 
 export default combineReducers({
-    song: songsReducer,
-    selectedSong : selectedSongReducer
-})
+	song: songsReducer,
+	selectedSong: selectedSongReducer
+});
